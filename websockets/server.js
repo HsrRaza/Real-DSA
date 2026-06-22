3a386cdd24dd18d4c2051d3807fac949afc80422
HsrRaza <hr.raza04@gmail.com>
2026-06-22 21:57:06 +0530

    websocket server

--- websockets/server.js
+++ websockets/server.js
@@ -1,10 +1,35 @@
-import http from "node:http"
+import http from "node:http";
+import { WebSocketServer } from "ws";
+import fs from "node:fs/promises";
+import path from "node:path";
 
-const  port  = process.env.PORT ?? 9000;
+const port = process.env.PORT ?? 9000;
 
-const httpServer = http.createServer(async function(req, res) {})
+const httpServer = http.createServer(async (req, res) => {
 
-httpServer.listen(port ,()=>{
-    console.log(`server is  running on https://localhost:${port}`);
-    
-})
+    const indexFile = await fs.readFile(
+        path.resolve("./index.html"),
+        "utf-8"
+    );
+
+    res.setHeader("Content-Type", "text/html");
+
+    res.end(indexFile);
+});
+
+const wsServer = new WebSocketServer({
+    server: httpServer
+});
+
+wsServer.on("connection", (websocket) => {
+
+    console.log("WebSocket Connected");
+
+    websocket.on("message", (data) => {
+        console.log(data.toString());
+    });
+});
+
+httpServer.listen(port, () => {
+    console.log(`Server running on http://localhost:${port}`);
+});
